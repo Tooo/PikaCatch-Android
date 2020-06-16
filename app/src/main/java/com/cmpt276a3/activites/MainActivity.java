d5d41e4bca0fc319353066ebabd7394e55fecf25
Toooo123 <34826963+Toooo123@users.noreply.github.com>
2020-06-16 16:59:48 -0700

    Initial Files
    Android Studio Made files

---
+++ app/src/main/java/com/cmpt276a3/activites/MainActivity.java
@@ -0,0 +1,16 @@
+package com.cmpt276a3.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cmpt276a3.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
