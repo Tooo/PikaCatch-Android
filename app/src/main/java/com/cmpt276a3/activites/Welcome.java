eb48217e3d485e3cb8e23f5ef1de86e555cc70c6
tsc7 <tsc7@sfu.ca>
2020-06-23 12:15:51 -0700

    Author, Delay, and Images
    Added my name, 3 second delay, and pokeball/pikachu image

--- app/src/main/java/com/cmpt276a3/activites/Welcome.java
+++ app/src/main/java/com/cmpt276a3/activites/Welcome.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.cmpt276a3.R;
 
 public class Welcome extends AppCompatActivity {
+    private boolean isActivityDone = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +20,8 @@ public class Welcome extends AppCompatActivity {
         setContentView(R.layout.activity_welcome);
 
         setupSkipButton();
-
+        animateImages();
+        addDelay();
     }
 
     private void setupSkipButton() {
@@ -26,10 +30,36 @@ public class Welcome extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Welcome.this, MainMenu.class);
-                finish();
-                startActivity(intent);
+                isActivityDone = true;
+                exitWelcome();
             }
         });
+    }
+
+    private void animateImages() {
+        ImageView imagePikachu = findViewById(R.id.welc_imgPikachu);
+        ImageView imagePokeball = findViewById(R.id.welc_imgPokeball);
+
+
+    }
+
+    // Refer to StackOverflow
+    // https://stackoverflow.com/questions/37323110/how-to-use-delay-functions-in-android-studio/37323343#comment62167379_37323110
+    private void addDelay() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!isActivityDone) {
+                    exitWelcome();
+                }
+            }
+        }, 2000);
+
+    }
+
+    private void exitWelcome() {
+        Intent intent = new Intent(Welcome.this, MainMenu.class);
+        finish();
+        startActivity(intent);
     }
 }
